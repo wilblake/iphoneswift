64feebd3971c7220ada5dbaecfef38ad774bb640
wil blake <wblake@emsys.net>
2018-10-27 12:48:25 -0400

    Initial Commit

---
+++ Lesson17/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson17
+//
+//  Created by wil blake on 10/27/18.
+//  Copyright © 2018 wil blake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
