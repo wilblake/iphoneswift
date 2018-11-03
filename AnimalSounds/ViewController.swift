b75b6bcc52d4f5516e16d69d3c692b002f7b27b2
Wil B <wblake@emsys.net>
2018-11-02 22:27:02 -0400

    Initial Commit

---
+++ AnimalSounds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalSounds
+//
+//  Created by wil blake on 11/2/18.
+//  Copyright © 2018 wilblake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
