83de2323e769bb9e3508dd7102f21b6302e89083
Wil B <wblake@emsys.net>
2018-11-02 22:24:28 -0400

    OK

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -13,8 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
     }
 
-
+    let elementList = ["Carbon","Gold","Chlorine","Sodium"]
+    var currentElementIndex = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func showAnswe(_ sender: UIButton) {
+        answerLabel.text=elementList[currentElementIndex]
+    }
+    @IBAction func gotoNextElement(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        updateElement()
+    }
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named:elementName)
+        imageView.image = image
+        
+    }
 }
 
