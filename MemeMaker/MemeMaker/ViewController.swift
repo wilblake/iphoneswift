a8180d38a39a75de73e6ab1b3139604674475cbf
wil <wilblakebr@gmail.com>
2018-11-15 22:12:53 -0500

    One more tim

--- MemeMaker/MemeMaker/ViewController.swift
+++ MemeMaker/MemeMaker/ViewController.swift
@@ -27,8 +27,37 @@ class ViewController: UIViewController {
         let coolChoice = CaptionChoice(emoji:"🕶", caption: "What's Cool?")
         let madChoice = CaptionChoice(emoji:"💥", caption: "Makes me mad...")
         let loveChoice = CaptionChoice(emoji:"❣️",caption: "Love Love ... ")
+        topChoices = [coolChoice,madChoice,loveChoice]
+        topCaptionSegmentedControl.removeAllSegments()
+        for choice in topChoices{
+            topCaptionSegmentedControl.insertSegment(withTitle: choice.emoji, at: topChoices.count, animated: false)
+        }
+        topCaptionSegmentedControl.selectedSegmentIndex = 0
+        
+        let catChoice = CaptionChoice(emoji: "😸", caption:  "Cats Wearing Hats")
+        let dogChoice = CaptionChoice(emoji: "🐶", caption: "Dogs Carrying Logs")
+        let monkeyChoice = CaptionChoice(emoji: "🐵", caption: "Monkey's Being Brass Funky")
+        bottomChoices = [catChoice,dogChoice,monkeyChoice]
+        bottomCaptionSegmentedControl.removeAllSegments()
+        
+        for choice in bottomChoices {
+            bottomCaptionSegmentedControl.insertSegment(withTitle: "choice.emoji", at: bottomChoices.count, animated: false)
+        }
+        bottomCaptionSegmentedControl.selectedSegmentIndex = 0
+        updateLabels()
+        
     }
 
+    func updateLabels() {
+        let topIndex = topCaptionSegmentedControl.selectedSegmentIndex
+        let bottomIndex = bottomCaptionSegmentedControl.selectedSegmentIndex
+        let topChoice = topChoices[topIndex]
+        let bottomChoice = bottomChoices[bottomIndex]
+        topCaptionLabel.text = topChoice.caption
+        bottomCaptionLabel.text = bottomChoice.caption
+    }
 
+    @IBAction func choiceSelected(_ sender: Any) {
+    }
 }
 
